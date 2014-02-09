65ed93a824b9134dc763b043f96fa347bc9b4886
yuanyangwu <yuanyang_wu@hotmail.com>
2014-02-09 11:12:52 +0800

    mine code to modify names

--- ch04-hot-dog-places/mine.R
+++ ch04-hot-dog-places/mine.R
@@ -3,14 +3,29 @@ library("ggplot2")
 setwd("C:/Users/wuf2/Documents/GitHub/note-on-visualize-this/ch04-hot-dog-places")
 hot_dog_places <- read.csv("hot-dog-places.csv", sep=",", header=TRUE)
 
-# read.csv prepends numeric headers with "X". For example, 2000 changes to X2000
-# assign names(v) to change headers 
-names(hot_dog_places) <- c("2000", "2001", "2002", "2003", "2004",
-  "2005", "2006", "2007", "2008", "2009", "2010")
-
-hot_dog_matrix <- as.matrix(hot_dog_places)
-
 func <- function(is.book) {
+
+  # change headers to remove confusing "X"
+  # read.csv prepends numeric headers with "X".
+  # For example, "2000" changes to "X2000"
+  if (is.book) {
+
+    names(hot_dog_places) <- c("2000", "2001", "2002", "2003", "2004",
+      "2005", "2006", "2007", "2008", "2009", "2010")
+
+  } else {
+
+    # option 1: replace "X" with ""
+    names(hot_dog_places) <- sub("X", "", names(hot_dog_places))
+
+    # option 2: assign generated string names from numeric
+    names(hot_dog_places) <- c(paste("200", 0:9, sep = ""), "2010")
+
+  }
+
+  hot_dog_matrix <- as.matrix(hot_dog_places)
+
+  # plot
   if (is.book) {
 
     barplot(hot_dog_matrix, border=NA, space=0.25, ylim=c(0, 200),
